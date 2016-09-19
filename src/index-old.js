0fb1b24f96ad4983ce2aff734499d400f3a7dfce
Olivier Lalonde <olalonde@gmail.com>
2016-09-19 11:05:18 -0700

    respect store.minPartSize

--- src/index-old.js
+++ src/index-old.js
@@ -1,14 +1,14 @@
+// THIS TEST FILE DOES NOT RESPECT THE STORE MINPARTSIZE CONFIGURATION!
+// (OBSOLETE) OR JUST ENABLE THOSE TESTS FOR STORES THAT DONT HAVE MINPARTSIZE
+
 import test from 'blue-tape'
 import str from 'string-to-stream'
 import concat from 'concat-stream'
-import { RandomStream } from 'common-streams'
 
 // TODO: make sure partial uploads are not visiable through createReadStream
+// TODO: remove { size } hint
 
 const noop = () => Promise.resolve()
-
-// Default to 2
-const minPartSize = (store) => (typeof store.minPartSize === 'undefined' ? 2 : store.minPartSize)
 
 export default ({
   setup = noop,
@@ -30,7 +30,7 @@ export default ({
       })
   })
 
-  test('info (foo) - unknown key', (t) => {
+  test('info - foo- unknown key', (t) => {
     store
       .info('foo')
       .catch((err) => {
@@ -39,9 +39,9 @@ export default ({
       })
   })
 
-  test('create foo (minPartSize + 1)', () => (
+  test('create foo', () => (
     store
-      .create('foo', { uploadLength: minPartSize(store) + 1 })
+      .create('foo', { uploadLength: 'bar'.length })
   ))
 
   test('info foo', (t) => (
@@ -49,37 +49,34 @@ export default ({
       .info('foo')
       .then(({ uploadOffset, uploadLength }) => {
         t.equal(uploadOffset, 0)
-        t.equal(uploadLength, minPartSize(store) + 1)
+        t.equal(uploadLength, 3)
       })
   ))
 
-  const randomChunks = []
-  test('write random data (minPartSize bytes) to foo', () => {
-    const rand = new RandomStream(minPartSize(store))
-    rand.pipe(concat((chunk) => { randomChunks.push(chunk) }))
-    return store.write('foo', rand)
-  })
+  test('write ba to foo (with size)', () => (
+    store
+      .write('foo', str('ba'), { size: 2 })
+  ))
 
   test('info foo', (t) => (
     store
       .info('foo')
       .then(({ uploadOffset, uploadLength }) => {
-        t.equal(uploadOffset, minPartSize(store))
-        t.equal(uploadLength, minPartSize(store) + 1)
+        t.equal(uploadOffset, 2)
+        t.equal(uploadLength, 3)
       })
   ))
 
-  test('write ! to foo', () => {
-    randomChunks.push(Buffer.from('!'))
-    return store.write('foo', str('!'))
-  })
+  test('write r to foo (with size)', () => (
+    store.write('foo', str('r'), { size: 1 })
+  ))
 
   test('info foo', (t) => (
     store
       .info('foo')
       .then(({ uploadOffset, uploadLength }) => {
-        t.equal(uploadOffset, minPartSize(store) + 1)
-        t.equal(uploadLength, minPartSize(store) + 1)
+        t.equal(uploadOffset, 3)
+        t.equal(uploadLength, 3)
       })
   ))
 
@@ -89,7 +86,7 @@ export default ({
       .createReadStream('foo')
       .on('error', t.error)
       .pipe(concat((buf) => {
-        t.deepEqual(buf, Buffer.concat(randomChunks))
+        t.equal(buf.toString(), 'bar')
         t.end()
       }))
   })
